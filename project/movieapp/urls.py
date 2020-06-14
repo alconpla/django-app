0e79da517f7a1bba303bca666e1ad6e77695b56c
AlexLeandoer <alconpla@gmail.com>
2020-06-14 19:27:18 +0200

    login css

--- project/movieapp/urls.py
+++ project/movieapp/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('movie/<str:title>', views.movie_detail, name='movie_detail'),
-    path('login', LoginView.as_view(), name='login'),
+    path('login', auth_views.LoginView.as_view(), name='login'),
 ]
